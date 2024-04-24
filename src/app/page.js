43e3a6ac4e47eb5010cbe0589ddf57ba6ece8405
Taiwo Akerele <davcodes24@gmail.com>
2024-04-24 04:45:16 -0700

    I love what i'm learning

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
-export default function Home() {
+import FormInput from "@/components/FormInput";
+export default function page() {
   return (
-    <div>Home</div>
+    <div className=" p-10 bg-white mt-10 mb-10 max-w-3xl m-auto rounded-lg">
+      <h1 className="text-center font-bold text-2xl">Send me a message</h1>
+      <p className="text-center mt-4 mb-4 text-gray-600">
+        Feel free to get in touch with me with anything related to CODINGSPACE{" "}
+        <br /> or you can just say hi. I will get back to you as soon as I can.
+      </p>
+      <form className=" px-24 pb-20">
+        <FormInput type="text" placeholder="Your name" />
+        <FormInput type="email" placeholder="Email" />
+        <FormInput type="text" placeholder="Subject" />
+        <textarea
+          placeholder="Message"
+          className=" active:border-blue-500 visited:border-blue-500 focus:border-blue-500 outline-none border-gray-500 border-2 text-blue-500 border-opacity-10 rounded-lg mt-6 mb-3 p-2 w-full"
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button className="bg-gradient-to-r from-blue-400 to-indigo-500 w-full px-5 py-4 text-white rounded-lg">Send</button>
+      </form>
+    </div>
   );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react"
+export default function FormInput({type, placeholder}) {
+    const [value, setValue] = useState('');
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+  return (
+    <>
+    <input className=" active:border-blue-500 visited:border-blue-500 focus:border-indigo-300 outline-none border-2 border-gray-500 text-indigo-400 border-opacity-10 rounded-lg w-full p-6 mt-6" type={type} placeholder={placeholder} value={value} onChange={handleChange} required />
+    </>
+  )
+}
